ed865f1ac64d8e6c90a9fbe59946c1a30d001a9f
Marcin Gajewski <code@gajewsky.it>
2020-04-29 10:59:16 +0200

    Update expense

--- src/app/bills/events.cljs
+++ src/app/bills/events.cljs
@@ -7,4 +7,14 @@
     (let [bill-id (get-in db [:nav :active-bill])]
       (update-in db [:bills bill-id :expenses] dissoc expense-id))))
 
+(reg-event-db
+  :upsert-expense
+  (fn [db [_ {:keys [id description value subcategory-id track?]}]]
+    (let [bill-id (get-in db [:nav :active-bill])]
+      (assoc-in db [:bills bill-id :expenses id] {:id id
+                                                  :description description
+                                                  :value value
+                                                  :subcategory-id subcategory-id
+                                                  :track? track?}))))
+
 
--- src/app/bills/views/bill_expenses.cljs
+++ src/app/bills/views/bill_expenses.cljs
@@ -1,19 +1,44 @@
 (ns app.bills.views.bill-expenses
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :as rf]
+            [reagent.core :as r]
+            [app.components.form-group :refer [form-group]]))
 
 (defn bill-expenses
   []
   (fn []
     (let [expenses @(rf/subscribe [:expenses])]
       [:div {:class "cards"}
-       (for [{:keys [id description]} expenses]
-         ^{:key id}
+       (for [{:keys [id] :as initial-values} expenses]
+         (let [values (r/atom initial-values)
+               save (fn [{:keys [id description value subcategory-id track?]}]
+                      (rf/dispatch [:upsert-expense {:id (or id (keyword (str "exp-" (random-uuid))))
+                                                     :description description
+                                                     :value (js/parseInt value)
+                                                     :subcategory-id subcategory-id
+                                                     :track? (boolean track?)}])
+                      (reset! values initial-values))]
 
-         [:<>
-          [:div description]
-          [:a {:href "#"
-               :on-click #(when (js/confirm "Are you sure?")
-                            (rf/dispatch [:delete-expense id]))}
-           "Delete"]])])))
+           ^{:key id}
 
+           [:<>
+            [form-group {:id :description
+                         :label "Description"
+                         :type "text"
+                         :values values}]
+            [form-group {:id :value
+                         :label "Value"
+                         :type "number"
+                         :values values}]
+            [form-group {:id :subcategory-id
+                         :label "Category"
+                         :type "text"
+                         :values values}]
+            [form-group {:id :track?
+                         :label "Track?"
+                         :type "text"
+                         :values values}]
+            [:button {:on-click #(save @values)} "Save"]
+            [:button {:on-click #(when (js/confirm "Are you sure?")
+                                   (rf/dispatch [:delete-expense id]))}
+             "Delete"]]))])))
 
