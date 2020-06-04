6fffaa3836662c88f42d30ae4ece0fa5c59d1453
Marcin Gajewski <code@gajewsky.it>
2020-06-04 14:52:10 +0200

    Fix not loading effects

--- src/app/effects.cljs
+++ src/app/effects.cljs
@@ -2,20 +2,17 @@
   (:require [re-frame.core :refer [reg-fx dispatch]]
             ["firebase/app" :as firebase]))
 
-(def firestore
-  (.firestore firebase))
-(.log js/console "dupa")
 (reg-fx
   :persist
   (fn [{:keys [path attrs]}]
-    (let [doc-ref (.doc firestore path)]
+    (let [firestore (.firestore firebase)
+          doc-ref (.doc firestore path)]
       (.set doc-ref (clj->js attrs) #js {:merge true}))))
 
-; (.log js/console "dupa2")
 (reg-fx
   :fetch
   (fn [{:keys [path on-success]}]
-
-    (let [col-ref (.collection firestore path)]
+    (let [firestore (.firestore firebase)
+          col-ref (.collection firestore path)]
       (-> (.get col-ref)
           (.then (fn [col-snap] (dispatch (conj on-success col-snap))))))))
