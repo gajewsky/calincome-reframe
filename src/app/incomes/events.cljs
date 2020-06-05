209eb7651b8ed5f3f194765b40d711da398174a7
Marcin Gajewski <code@gajewsky.it>
2020-06-05 18:47:47 +0200

    Clean incomes events

--- src/app/incomes/events.cljs
+++ src/app/incomes/events.cljs
@@ -3,48 +3,50 @@
             [clojure.walk :refer [keywordize-keys]]
             [nano-id.core :refer [nano-id]]))
 
+(def incomes-path "/incomes/")
+
+(defn income-path
+  [id]
+  (str incomes-path (name id)))
+
 (reg-event-fx
   :delete-income
-  (fn [{:keys [db]} [_ income-id]]
-    (let [income-path (str "/incomes/" (name income-id))]
-      {:db (update-in db [:incomes] dissoc income-id)
-       :persist-delete {:path income-path}})))
+  (fn [{:keys [db]} [_ id]]
+    {:db (update-in db [:incomes] dissoc id)
+     :persist-delete {:path (income-path id)}}))
 
 (reg-event-fx
   :update-income
-  (fn [{:keys [db]} [_ {:keys [id user-id inc-category date value]}]]
-    (let [income-id (get-in db [:nav :active-income])
-          incomes-path "/incomes/"
-          income-path (str "/incomes/" (name income-id))
-          income-attrs {:id id
-                        :user-id user-id
-                        :inc-category inc-category
-                        :value value
-                        :date date }]
+  (fn [{:keys [db]} [_ {:keys [user-id inc-category date value]}]]
+    (let [id (get-in db [:nav :active-income])
+          attrs {:id id
+                 :user-id user-id
+                 :inc-category inc-category
+                 :value value
+                 :date date }]
 
-      {:db (update-in db [:incomes income-id] merge income-attrs)
+      {:db (update-in db [:incomes id] merge attrs)
        :navigate-to {:path incomes-path}
-       :persist {:path income-path :attrs income-attrs}})))
+       :persist {:path (income-path id) :attrs attrs}})))
 
 (reg-event-fx
   :create-income
   (fn [{:keys [db]} [_ user-id]]
-    (let [income-id (keyword (nano-id 10))
+    (let [id (keyword (nano-id 10))
           time-now (.now js/Date)
-          income-path (str "/incomes/" (name income-id))
-          income-attrs {:id income-id
-                        :user-id user-id
-                        :inc-category ""
-                        :value 0
-                        :date time-now
-                        :created-at time-now}]
-      {:db (assoc-in db [:incomes income-id] income-attrs)
-       :navigate-to {:path income-path}})))
+          attrs {:id id
+                 :user-id user-id
+                 :inc-category ""
+                 :value 0
+                 :date time-now
+                 :created-at time-now}]
+      {:db (assoc-in db [:incomes id] attrs)
+       :navigate-to {:path (income-path id)}})))
 
 (reg-event-fx
   :fetch-incomes
   (fn [&_]
-    {:fetch {:path "/incomes/" :on-success [:fetch-incomes-success]}}))
+    {:fetch {:path incomes-path :on-success [:fetch-incomes-success]}}))
 
 (defn index-by
   "Transform a coll to a map with a given key as a lookup value"
@@ -57,11 +59,11 @@
   :fetch-incomes-success
   (fn [db [_ col-snap]]
     (let [incomes (->> (.-docs col-snap)
-                      (map #(.data %))
-                      (map #(js->clj %))
-                      (map keywordize-keys)
-                      vec
-                      (index-by :id))]
+                       (map #(.data %))
+                       (map #(js->clj %))
+                       (map keywordize-keys)
+                       vec
+                       (index-by :id))]
 
       (assoc db :incomes incomes))))
 
